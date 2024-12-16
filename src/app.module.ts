7dcc0ae7e31c2c5ec6a185c95728519e6848b047
chinsanchung <chinsanchung@gmail.com>
2024-12-16 23:26:29 +0900

    [가계부] feat: typeorm 으로 로컬 mysql 에 연결
    - TypeOrmModule 을 app.module.ts 의 imports 에 담았습니다.
    - port 나 username 등 관련 설정은 .env 파일에서 가져옵니다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER_NAME,
+      password: process.env.MYSQL_USER_PASSWORD,
+      database: process.env.MYSQL_DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
