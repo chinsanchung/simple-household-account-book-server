d9918ae7b7faf877c635ec7f8a77066b1cbfa524
chinsanchung <chinsanchung@gmail.com>
2024-12-16 23:37:18 +0900

    [가계부] fix: 유저 - bcrypt 패키지를 사용해 비밀번호를 암호화해서 저장, registeredDate 컬럼을 등록하지 않던 것을 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -11,10 +12,11 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  create({ userId, password }: CreateUserDto) {
+  async create({ userId, password }: CreateUserDto) {
     const user = new User();
     user.userId = userId;
-    user.password = password;
+    user.password = await bcrypt.hash(password, 10);
+    user.registeredDate = new Date();
 
     return this.usersRepository.save(user);
   }
