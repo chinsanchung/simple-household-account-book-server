1894f88e3b000fefbdab4f7289be1f8e354f5dff
chinsanchung <chinsanchung@gmail.com>
2024-12-17 22:17:28 +0900

    [가계부] error: 유저 - 유저 엔티티에서, userId 에 unique 제약 조건을 추가

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column } from 'typeorm';
 
 @Entity()
 export class User {
-  @Column({ primary: true, nullable: false, length: 15 })
+  @Column({ primary: true, nullable: false, length: 15, unique: true })
   userId: string;
 
   @Column({ nullable: false })
