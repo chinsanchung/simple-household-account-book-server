4bc3313526f7703ca9871b02cfc229afcbf27a5d
chinsanchung <chinsanchung@gmail.com>
2024-12-17 22:31:02 +0900

    [가계부] refactor: 유저 - 비밀번호를 암호화할 때 솔트 값을 bcrypt.genSalt 로 생성하도록 설정
    - 매번 계정의 비밀번호를 생성할 때마다 고유한 솔트를 사용하는 방식으로 수정합니다.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
@@ -13,9 +13,17 @@ export class UsersService {
   ) {}
 
   async create({ userId, password }: CreateUserDto) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { userId },
+    });
+    if (existingUser) {
+      throw new ConflictException('이미 같은 이름의 아이디가 있습니다.');
+    }
+
     const user = new User();
     user.userId = userId;
-    user.password = await bcrypt.hash(password, 10);
+    const salt = await bcrypt.genSalt(12);
+    user.password = await bcrypt.hash(password, salt);
     user.registeredDate = new Date();
 
     return this.usersRepository.save(user);
