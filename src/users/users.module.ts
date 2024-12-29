f208e2fef9c43003aa959f003b637bfdcdac24e6
chinsanchung <chinsanchung@gmail.com>
2024-12-29 16:56:44 +0900

    [가계부] fix: 유저 - 유저를 생성하는 create 서비스 메소드에 logger 를 적용
    - 에러가 발생할 가능성이 있는 부분에 try/catch, 그리고 logger 를 적용했습니다.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
+import { CustomLoggerModule } from '../logger/logger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), CustomLoggerModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,29 +5,57 @@ import * as bcrypt from 'bcrypt';
 import 'dotenv/config';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { CustomLoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
-  ) {}
+    private readonly logger: CustomLoggerService,
+  ) {
+    this.logger.setContext(UsersService.name);
+  }
 
   async create({ userId, password }: CreateUserDto) {
-    const existingUser = await this.usersRepository.findOne({
-      where: { userId },
-    });
-    if (existingUser) {
-      throw new ConflictException('이미 같은 이름의 아이디가 있습니다.');
+    try {
+      const existingUser = await this.usersRepository.findOne({
+        where: { userId },
+      });
+      if (existingUser) {
+        this.logger.warn(
+          `USER CREATE - Attempted to create duplicate user: ${userId}`,
+        );
+        throw new ConflictException('이미 같은 이름의 아이디가 있습니다.');
+      }
+
+      const user = new User();
+      user.userId = userId;
+      try {
+        const salt = await bcrypt.genSalt(
+          parseInt(process.env.BCRYPT_SALT_ROUND),
+        );
+        user.password = await bcrypt.hash(password, salt);
+      } catch (error) {
+        this.logger.error(
+          `USER CREATE - Password hashing failed for user ${userId}`,
+          error.stack,
+        );
+        throw new Error('비밀번호 암호화 중 오류가 발생했습니다.');
+      }
+      user.registeredDate = new Date();
+
+      await this.usersRepository.save(user);
+      return '아이디 생성에 성공했습니다.';
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+      this.logger.error(
+        `USER CREATE - Failed to create user ${userId}`,
+        error?.stack,
+      );
+      throw new Error('사용자 생성 중 오류가 발생했습니다.');
     }
-
-    const user = new User();
-    user.userId = userId;
-    const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUND));
-    user.password = await bcrypt.hash(password, salt);
-    user.registeredDate = new Date();
-
-    await this.usersRepository.save(user);
-    return '아이디 생성에 성공했습니다.';
   }
 }
