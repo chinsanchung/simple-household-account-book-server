7a5c09a022a463f542dd53d0cf60d3253e633c45
chinsanchung <chinsanchung@gmail.com>
2024-12-31 22:11:32 +0900

    [가계부] fix: 로그인 - 토큰의 sub 값을 userName 컬럼에서 다른 것으로 변경
    - Date.getTime 메소드 값을 sub 로 설정했습니다.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,8 +24,7 @@ export class AuthService {
   async login(user: User) {
     const payload = {
       username: user.userName,
-      sub: user.userName,
-      time: new Date().getTime(),
+      sub: new Date().getTime(),
     };
     return {
       access_token: this.jwtService.sign(payload),
