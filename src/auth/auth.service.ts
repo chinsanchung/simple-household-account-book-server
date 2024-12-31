f9220097a05038a4090efc5b76ae95ea918bf61d
chinsanchung <chinsanchung@gmail.com>
2024-12-31 17:27:15 +0900

    [가계부] feat: '/auth/login' api 에서 로그인 토큰을 발급하도록 설정, jwt guard 를 작성
    - 공식 문서를 참고해서 토큰을 발급하는 기능 그리고 토큰을 검증하는 가드를 만들었습니다.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOneByUserId(username);
@@ -14,5 +19,16 @@ export class AuthService {
       else return null;
     }
     return null;
+  }
+
+  async login(user: User) {
+    const payload = {
+      username: user.userId,
+      sub: user.userId,
+      time: new Date().getTime(),
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
